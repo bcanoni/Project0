6b973c06154057e326f9bd1add1c33d00d09ad04
Brian Canoni <bcanoni@gmail.com>
2015-10-18 19:29:05 -0400

    Barebones PCB
    Not used anywhere yet

---
+++ distrib/os/pcb.js
@@ -0,0 +1,11 @@
+function Pcb() {
+    var pcbPid = pid++;
+    var pc = 0; // Program Counter
+    var acc = 0; // Accumulator
+    var xReg = 0; // X register
+    var yReg = 0; // Y register
+    var zFlag = 0; // Z-ero flag 	
+    var base = 0; // Starting location of memory in dec?
+    var limit = 0; // Maximum location in memory in dec?
+}
+var pid = 0;
