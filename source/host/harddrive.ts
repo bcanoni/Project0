d31efae25a5d153a19eccf082fb254c4a32d1273
Brian Canoni <bcanoni@gmail.com>
2015-12-13 05:07:39 -0500

    Added outline for harddrive
    -Trying to figure out what I need there

---
+++ source/host/harddrive.ts
@@ -0,0 +1,33 @@
+///<reference path="../globals.ts" />
+
+
+
+module TSOS 
+{
+
+    export class HardDrive
+	{
+		private diskDrive 
+		
+		
+		//What is a track?
+		//what is a sector?
+		//what is a block?
+		//what do i need for my program
+		
+		 constructor(public tracks number = 0; 
+					 public sectors number = 0;
+					 public blocks number = 128;	 
+		 
+					 ){}
+					 
+					 
+		//Some kind of access
+		
+		
+	
+	}
+	
+	
+}
+
