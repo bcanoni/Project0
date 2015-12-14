dddb4b61c33ce5881706205518346083d917edad
Brian Canoni <bcanoni@gmail.com>
2015-12-14 02:07:41 -0500

    Basic Outline of DiskManager

---
+++ source/os/diskManager.ts
@@ -0,0 +1,39 @@
+/// <reference path="../host/harddrive.ts"/>
+
+/*
+Brian Canoni
+DiskManager
+*/
+
+module TSOS 
+{
+    export class DiskManager
+	{
+	
+	
+		constructor(){}
+		
+		public init()
+		{
+			initHardDriveTable();
+		
+		}
+		
+		
+		public updateHardDriveTable()
+		{
+		
+		}
+		
+		
+		public initHardDriveTable()
+		{
+		
+		
+		}
+	
+	
+	}
+	
+	
+}
