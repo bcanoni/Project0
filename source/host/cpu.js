7d191e36a39e0d00b22ee76c22a52203e6a42d22
Brian Canoni <bcanoni@gmail.com>
2015-12-14 14:50:36 -0500

    Attempt to update HD table real time
    -cpu cycle based udpates
    -trying to write data in hex

---
+++ source/host/cpu.js
@@ -0,0 +1,251 @@
+///<reference path="../globals.ts" />
+/* ------------
+     CPU.ts
+
+     Requires global.ts.
+
+     Routines for the host CPU simulation, NOT for the OS itself.
+     In this manner, it's A LITTLE BIT like a hypervisor,
+     in that the Document environment inside a browser is the "bare metal" (so to speak) for which we write code
+     that hosts our client OS. But that analogy only goes so far, and the lines are blurred, because we are using
+     TypeScript/JavaScript in both the host and client environments.
+
+     This code references page numbers in the text book:
+     Operating System Concepts 8th edition by Silberschatz, Galvin, and Gagne.  ISBN 978-0-470-12872-5
+     ------------ */
+var TSOS;
+(function (TSOS) {
+    var Cpu = (function () {
+        function Cpu(PC, Acc, Xreg, Yreg, Zflag, isExecuting) {
+            if (PC === void 0) { PC = 0; }
+            if (Acc === void 0) { Acc = 0; }
+            if (Xreg === void 0) { Xreg = 0; }
+            if (Yreg === void 0) { Yreg = 0; }
+            if (Zflag === void 0) { Zflag = 0; }
+            if (isExecuting === void 0) { isExecuting = false; }
+            this.PC = PC;
+            this.Acc = Acc;
+            this.Xreg = Xreg;
+            this.Yreg = Yreg;
+            this.Zflag = Zflag;
+            this.isExecuting = isExecuting;
+        }
+        Cpu.prototype.init = function () {
+            _CPU = this;
+            this.PC = 0;
+            this.Acc = 0;
+            this.Xreg = 0;
+            this.Yreg = 0;
+            this.Zflag = 0;
+            this.isExecuting = false;
+        };
+        Cpu.prototype.cycle = function () {
+            // TODO: Accumulate CPU usage and profiling statistics here.
+            // Do the real work here. Be sure to set this.isExecuting appropriately.
+            //6D ADC add with carry adds constants of address to the contents of accumulator and puts results in accumulator
+            //A2 LDX loads x register with a constant
+            //AE LDX loads the X register from memory
+            //A0 LDY loads y register with a constant 
+            //AC loads the y register from memory
+            //EA NOP no operation
+            //00 Break (really a system call)
+            //EC CPX compare a byte in memory to x regi sets the z zero flag if equal 
+            //D0 BNE branch n bytes if z flag is 0-470-12872-5
+            //EE INC increment the value of a byte
+            //FF SYS system call
+            //IR means 
+            var ir;
+            var i;
+            var a;
+            var b;
+            _Kernel.krnTrace('CPU cycle');
+            if (this.isExecuting) {
+                ir = _Memory.Data[this.PC];
+                //ir = _MemManager.getMemory(_PCB.PC);
+                //alert(ir + "@" + this.PC);
+                //step by step loool
+                switch (ir) {
+                    case "A9":
+                        this.PC++;
+                        this.Acc = parseInt(_MemManager.getMemory(this.PC), 16);
+                        this.PC++;
+                        break;
+                    case "AD":
+                        var byteOne = _MemManager.getMemory(this.PC + 1);
+                        var byteTwo = _MemManager.getMemory(this.PC + 2);
+                        var hexAddress = (byteTwo + byteOne);
+                        var decAddress = _MemManager.toAddress(hexAddress);
+                        //i=_MemManager.toAddress();
+                        this.Acc = parseInt(_MemManager.getMemory(decAddress), 16);
+                        this.PC++;
+                        break;
+                    case "8D":
+                        var byteOne = _MemManager.getMemory(this.PC + 1);
+                        var byteTwo = _MemManager.getMemory(this.PC + 2);
+                        var hexAddress = (byteTwo + byteOne);
+                        var decAddress = _MemManager.toAddress(hexAddress);
+                        _MemManager.insertMemory(decAddress, this.Acc.toString(16));
+                        this.PC++;
+                        break;
+                    case "6D":
+                        var byteOne = _MemManager.getMemory(this.PC + 1);
+                        var byteTwo = _MemManager.getMemory(this.PC + 2);
+                        var hexAddress = (byteTwo + byteOne);
+                        var decAddress = _MemManager.toAddress(hexAddress);
+                        this.Acc += parseInt(_MemManager.getMemory(decAddress), 16);
+                        this.PC++;
+                        break;
+                    case "A2":
+                        this.PC++;
+                        this.Xreg = parseInt(_MemManager.getMemory(this.PC), 16);
+                        this.PC++;
+                        break;
+                    case "AE":
+                        var byteOne = _MemManager.getMemory(this.PC + 1);
+                        var byteTwo = _MemManager.getMemory(this.PC + 2);
+                        var hexAddress = (byteTwo + byteOne);
+                        var decAddress = _MemManager.toAddress(hexAddress);
+                        this.Xreg = parseInt(_MemManager.getMemory(decAddress), 16);
+                        this.PC++;
+                        break;
+                    case "A0":
+                        this.PC++;
+                        this.Yreg = parseInt(_MemManager.getMemory(this.PC), 16);
+                        this.PC++;
+                        break;
+                    case "AC":
+                        var byteOne = _MemManager.getMemory(this.PC + 1);
+                        var byteTwo = _MemManager.getMemory(this.PC + 2);
+                        var hexAddress = (byteTwo + byteOne);
+                        var decAddress = _MemManager.toAddress(hexAddress);
+                        this.Yreg = parseInt(_MemManager.getMemory(decAddress), 16);
+                        this.PC++;
+                        break;
+                    case "EA":
+                        this.PC++;
+                        break;
+                    case "00":
+                        // this.isExecuting = false;
+                        _PCB.state = 3; //terminated
+                        _Scheduler.removeRow(1);
+                        _Scheduler.switcher();
+                        //this.PC=0;
+                        break;
+                    case "EC":
+                        var byteOne = _MemManager.getMemory(this.PC + 1);
+                        var byteTwo = _MemManager.getMemory(this.PC + 2);
+                        var hexAddress = (byteTwo + byteOne);
+                        var decAddress = _MemManager.toAddress(hexAddress);
+                        if (this.Xreg === parseInt(_MemManager.getMemory(decAddress)))
+                            this.Zflag = 0;
+                        else
+                            this.Zflag = 1;
+                        this.PC++;
+                        break;
+                    case "D0":
+                        //this.PC++;
+                        if (this.Zflag === 1) {
+                            //alert(parseInt(_MemManager.getMemory(this.PC+1),16)+1);
+                            this.PC += parseInt(_MemManager.getMemory(this.PC + 1), 16) + 2;
+                            var check = this.PC + _PCB.base;
+                            if (check >= _PCB.limit) {
+                                this.PC -= 256;
+                            }
+                        }
+                        else {
+                            this.PC++;
+                            this.PC++;
+                        }
+                        break;
+                    case "EE":
+                        var byteOne = _MemManager.getMemory(this.PC + 1);
+                        var byteTwo = _MemManager.getMemory(this.PC + 2);
+                        var hexAddress = (byteTwo + byteOne);
+                        var decAddress = _MemManager.toAddress(hexAddress);
+                        a = parseInt(_MemManager.getMemory(decAddress), 16);
+                        a = a + 1;
+                        _MemManager.insertMemory(decAddress, a.toString(16));
+                        this.PC++;
+                        break;
+                    case "FF":
+                        //_StdOut.putPrompt();
+                        if (this.Xreg == 1) {
+                            _StdOut.putText("" + this.Yreg);
+                            _StdOut.advanceLine();
+                            _OsShell.putPrompt();
+                            this.PC++;
+                        }
+                        else if (this.Xreg == 2) {
+                            //00 terminated
+                            var temp = true;
+                            var c = 0;
+                            while (temp) {
+                                _StdOut.putText("" + String.fromCharCode(parseInt(_MemManager.getMemory(this.Yreg + c), 16)));
+                                c++;
+                                temp = ("00" !== _MemManager.getMemory(this.Yreg + c));
+                            }
+                            _StdOut.advanceLine();
+                            _OsShell.putPrompt();
+                            this.PC++;
+                        }
+                        else {
+                            _StdOut.putText("Value in Xreg must be 1 or 0");
+                            this.isExecuting = false;
+                            _OsShell.putPrompt();
+                        }
+                        break;
+                    default:
+                        this.isExecuting = false;
+                        _StdOut.putText("missing code : " + _MemManager.getMemory(this.PC));
+                        _StdOut.advanceLine();
+                        _OsShell.putPrompt();
+                }
+                //Update cpu registers 
+                var cell = document.getElementById("pcDisplay");
+                cell.innerHTML = "" + this.PC;
+                cell = document.getElementById("accDisplay");
+                cell.innerHTML = "" + this.Acc;
+                cell = document.getElementById("xRegDisplay");
+                cell.innerHTML = "" + this.Xreg;
+                cell = document.getElementById("yRegDisplay");
+                cell.innerHTML = "" + this.Yreg;
+                cell = document.getElementById("zRegDisplay");
+                cell.innerHTML = "" + this.Zflag;
+                //UPDATE PCB REGISTERS
+                _PCB.PC = this.PC;
+                _PCB.Acc = this.Acc;
+                _PCB.Xreg = this.Xreg;
+                _PCB.Yreg = this.Yreg;
+                _PCB.Zflag = this.Zflag;
+                _PCB.state = 2; // RUNNING
+                _Scheduler.updatePCBTable();
+                _MemManager.updateMemoryTable();
+                _Scheduler.switcher();
+                _DiskManager.updateHardDriveTable();
+            }
+        };
+        Cpu.prototype.getConstantNumber = function (num) {
+            var v = parseInt(num, 16);
+            return v;
+        };
+        Cpu.prototype.clearCpu = function () {
+            this.PC = 0;
+            this.Acc = 0;
+            this.Xreg = 0;
+            this.Yreg = 0;
+            this.Zflag = 0;
+            this.isExecuting = false;
+        };
+        Cpu.prototype.switchTo = function (newpcb) {
+            _PCB = newpcb;
+            this.PC = newpcb.PC;
+            this.Acc = newpcb.Acc;
+            this.Xreg = newpcb.Xreg;
+            this.Yreg = newpcb.Yreg;
+            this.Zflag = newpcb.Zflag;
+            _Kernel.krnTrace('Switch to PID[' + _PCB.pid + ']');
+        };
+        return Cpu;
+    })();
+    TSOS.Cpu = Cpu;
+})(TSOS || (TSOS = {}));
--- source/os/diskManager.ts
+++ source/os/diskManager.ts
@@ -10,15 +10,11 @@ module TSOS
     export class DiskManager
 	{
 		
+
 	
-		constructor()
-		{	
-		//REM BLOCKS OF 2 CAUSE HEX
-		var headerLen = 4;
-		var dataLen = 60;
-		var fileNames = [];
-		
-		}
+		constructor(public headerLen = 4,
+					public dataLen = 60,
+					public fileNames = []){}
 		
 		public init()
 		{
@@ -28,41 +24,71 @@ module TSOS
 		
 		public createFile(fileName)
 		{
-			for(var fn : this.fileNames)
+			for(var x = 0; x < this.fileNames.length ; x++)
 			{
-				if(fn == fileName)
+				if(this.fileNames[x] == fileName)
 				{
 					//BAD!
 					return null;
 				}
-				
-				//if no space 
-				//return
-				
-				// if file too big
-				//cannot be > 60
-				//
-				
-				
-				//OTHERWISE SET THIS
-				
-				//find first next free spot
-				
-				
-				
-
-				return null;
-			
 			}
+				
+			//if no space 
+			//return
+			
+			// if file too big
+			//cannot be > 60
+			//
+			
+			
+			//OTHERWISE SET THIS
+			
+			//find first next free spot
+			this.fileNames.push(fileName); //at end of array
+			
+			var loc = this.nextFree();
+			if(loc != null)
+			{
+				_HardDrive.write(loc.substring(0,1),loc.substring(2,3),loc.substring(4,5),fileName);
+				return null;
+			}
+			
+			
+			
+
+			return null;	
+			
 		
 		
 		}
 		
 		public nextFree()
 		{
-			var data = this.getContent(0, 0, 0);
-			//var fileLocation = slice data and ;
-			return fileLocation;
+			var data;
+			
+			for(var t = 0 ; t <= 3 ; t++)
+			{
+				
+				
+				
+				for(var s = 0; s <= 7 ; s++)
+				{
+					for(var b = 0; b <= 7 ; b++)
+					{
+						data = _HardDrive.read(t,s,b);
+						if( data.substring(0,4) == "0000")
+						{
+							return t+":"+s+":"+b;
+						}
+					 
+					}
+					
+				}
+				
+			}
+			return null;
+			
+			
 		
 		}
 		
@@ -80,12 +106,31 @@ module TSOS
 		
 		public write(t,s,b, data)
 		{
-			return _HardDrive.write(t,s,b, data);
+			var hdata = data.toString(16);
+			return _HardDrive.write(t,s,b, hdata);
 		}
 		
 		
 		public updateHardDriveTable()
 		{
+			
+			
+			for(var t = 0 ; t <= 3 ; t++)
+			{		
+				for(var s = 0; s <= 7 ; s++)
+				{
+					for(var b = 0; b <= 7 ; b++)
+					{
+					var cell =  <HTMLTableDataCellElement>document.getElementById(t + ":" + s + ":" + b + "d");
+					
+					var updateData = _HardDrive.read(t,s,b);
+					cell.innerHTML = updateData;					
+					
+					}
+					
+				}
+				
+			}				
 		
 		}
 		
@@ -111,8 +156,10 @@ module TSOS
 					{
 					var cell = row.insertCell(0);
 					cell.innerHTML = t + ":" + s + ":" + b;
+					cell.id=t + ":" + s + ":" + b;
 					
 					cell = row.insertCell(1);
+					cell.id=t + ":" + s + ":" + b + "d";
 					
 					cell.innerHTML = (zero128);
 					_HardDrive.write(t,s,b,zero128);
