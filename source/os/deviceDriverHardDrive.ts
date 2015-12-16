a4a95b7436b034afbd4efd6863c82edd32003ef6
Brian Canoni <bcanoni@gmail.com>
2015-12-16 06:25:51 -0500

    Starting dev on HD driver
    -Also going to fix the write command

---
+++ source/os/deviceDriverHardDrive.ts
@@ -0,0 +1,24 @@
+///<reference path="../globals.ts" />
+///<reference path="deviceDriver.ts" />
+
+
+/** 
+ * 
+ * Brian Canoni 
+
+ */
+
+ module TSOS 
+ {
+
+    // Extends DeviceDriver
+    export class DeviceDriverKeyboard extends DeviceDriver 
+	{
+	
+	
+	
+	
+	
+	}
+	
+}
